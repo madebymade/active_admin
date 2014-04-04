70f602426ccee35ab826b730676652dc9243dee7
Seb Ashton <seb@sebashton.com>
2014-04-04 17:52:25 +0100

    Update active_admin.rb

--- lib/active_admin.rb
+++ lib/active_admin.rb
@@ -11,7 +11,6 @@ require 'sass-rails'
 require 'inherited_resources'
 require 'jquery-rails'
 require 'jquery-ui-rails'
-require 'coffee-rails'
 require 'arbre'
 
 require 'active_admin/helpers/i18n'
